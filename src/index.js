62ad0a4a26dcd5111cb0f0e9670654e999c6c1ca
MinhNQ <minhnq@timebird.org>
2024-01-18 11:20:35 +0700

    routes and controller

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path');
 
+const route = require('./routes');
+
 const app = express();
 const port = 3000;
 
 // HTTP logger
-// app.use(morgan('combined'));
+app.use(morgan('combined'));
 
 // 
+app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
 }));
-app.use(express.json());
 
 // static
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,22 +25,7 @@ app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', './src/resources/views');
 
-app.get('/', (req, res) => {
-  res.render('home')
-});
-
-app.get('/news', (req, res) => {
-  res.render('news')
-});
-
-app.get('/search', (req, res) => {
-  res.render('search');
-});
-
-app.post('/search', (req, res) => {
-  console.log('req.body = ', req.body);
-  res.send('');
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
