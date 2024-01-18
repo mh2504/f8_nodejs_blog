e272901ab4e205c5dabc131836888eb058410753
MinhNQ <minhnq@timebird.org>
2024-01-18 16:27:41 +0700

    install prettier lint-staged husky

--- src/index.js
+++ src/index.js
@@ -11,23 +11,24 @@ const port = 3000;
 // HTTP logger
 app.use(morgan('combined'));
 
-// 
+//
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true,
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
 // static
-app.use(express.static(path.join(__dirname, 'public')));
+        app.use(express.static(path.join(__dirname, 'public')));
 
 // Template engine
-app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+              app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', './src/resources/views');
 
-route(app);
+      route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
-
